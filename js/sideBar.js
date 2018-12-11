718b9778dbb9ae9b8707413f445a6261f28b6b00
ishmail66 <ishmail66@gmail.com>
2018-12-11 10:47:56 +0200

    created a new layout

--- js/sideBar.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-   if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-       document.getElementById("slider").style.display = "block";
-   } else {
-       document.getElementById("slider").style.display = "block";
-   }
-}
